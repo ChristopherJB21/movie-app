b1d0bafa324341c8c31516290d6d670d91be09d7
xtreme16 <cristianwijaya03@gmail.com>
2023-05-26 21:46:46 +0700

    menambahkan about us

--- resources/views/home/aboutUs.blade.php
+++ resources/views/home/aboutUs.blade.php
@@ -1,3 +1,7 @@
 @extends('layouts.app')
 @section('content')
+
+<!-- CODING DI SINI -->
+<h1 class = 'px-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi nulla corrupti nam ut molestias id, veritatis ipsam eius tempora odit illo officia porro doloribus, voluptatum cupiditate! Provident enim illo earum?</h1>
+
 @endsection
