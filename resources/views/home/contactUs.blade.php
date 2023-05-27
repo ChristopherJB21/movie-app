f4831599c82178784f620c63151a2a881975d5b6
MarioAnanta <anantamario1@gmail.com>
2023-05-27 12:13:33 +0700

    tambahin contact us masih kasar
    sama

--- resources/views/home/contactUs.blade.php
+++ resources/views/home/contactUs.blade.php
@@ -1,3 +1,35 @@
 @extends('layouts.app')
 @section('content')
+    <div>
+        <h1>contact us </h1>
+    </div>
+
+    <div class="container text-center">
+        <div class="row align-items-start">
+            <div class="col">
+                <h2>Email</h2>
+                <p>ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk membahas tetang masalah, pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda<p>
+            </div>
+            <div class="col">
+                <h2>Email</h2>
+                <p>ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk membahas tetang masalah, pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda<p>
+            </div>
+            <div class="col">
+                <h2>Email</h2>
+                <p>ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk membahas tetang masalah, pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda<p>
+            </div>
+        </div>
+    </div>
+    <div class="container text-center">
+        <div class="row align-items-start">
+            <div class="col-3 p-3 mb-2">
+                <h2>Email</h2>
+                <p>ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk membahas tetang masalah, pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda<p>
+            </div>
+            <div class="col-3 p-3 mb-2">
+                <h2>Email</h2>
+                <p>ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk membahas tetang masalah, pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda<p>   
+            </div>
+        </div>
+    </div>
 @endsection
