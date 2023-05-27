c4b315c0370a256ae39750865d899a29c0aec42b
Christopher Reswara <c1kalijaten@gmail.com>
2023-05-27 12:35:18 +0700

    udpate contact us
    update ke 2

--- resources/views/home/contactUs.blade.php
+++ resources/views/home/contactUs.blade.php
@@ -3,32 +3,48 @@
     <div>
         <h1>contact us </h1>
     </div>
-
-    <div class="container text-center">
-        <div class="row align-items-start">
-            <div class="col">
-                <h2>Email</h2>
-                <p>ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk membahas tetang masalah, pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda<p>
-            </div>
-            <div class="col">
-                <h2>Email</h2>
-                <p>ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk membahas tetang masalah, pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda<p>
-            </div>
-            <div class="col">
-                <h2>Email</h2>
-                <p>ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk membahas tetang masalah, pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda<p>
+    <div class="bg-dark text-white mx-0">
+        <div class="container text-center">
+            <div class="row justify-content-center">
+                <div class="col-3 mx-4 ">
+                    <h2>Email</h2>
+                    <p class="bg-purple">ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk membahas
+                        tetang masalah,
+                        pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda
+                    <p>
+                </div>
+                <div class="col-3 mx-4 ">
+                    <h2>Email</h2>
+                    <p class="bg-purple">ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk membahas
+                        tetang masalah,
+                        pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda
+                    <p>
+                </div>
+                <div class="col-3 mx-4 ">
+                    <h2>Email</h2>
+                    <p class="bg-purple">ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk membahas
+                        tetang masalah,
+                        pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda
+                    <p>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="container text-center">
-        <div class="row align-items-start">
-            <div class="col-3 p-3 mb-2">
-                <h2>Email</h2>
-                <p>ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk membahas tetang masalah, pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda<p>
-            </div>
-            <div class="col-3 p-3 mb-2">
-                <h2>Email</h2>
-                <p>ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk membahas tetang masalah, pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda<p>   
+        <div class="container text-center">
+            <div class="row justify-content-center">
+                <div class="col-3 mx-4 ">
+                    <h2>Email</h2>
+                    <p class="bg-purple">ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk
+                        membahas tetang masalah,
+                        pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda
+                    <p>
+                </div>
+                <div class="col-3 mx-4 ">
+                    <h2>Email</h2>
+                    <p class="bg-purple">ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk
+                        membahas tetang masalah,
+                        pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda
+                    <p>
+                </div>
             </div>
         </div>
     </div>
