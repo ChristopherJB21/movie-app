020384fca79bdaba468ceae15bf823fb56955a0a
Arata <Josuancls21@gmail.com>
2023-05-28 13:08:04 +0700

    nyoba dikit
    hehe

--- resources/views/home/contactUs.blade.php
+++ resources/views/home/contactUs.blade.php
@@ -1,26 +1,26 @@
 @extends('layouts.app')
 @section('content')
     <div>
-        <h1>contact us </h1>
+        <h1 class="bg-purple mx-3 text-info-emphasis ">contact us </h1>
     </div>
-    <div class="bg-dark text-white mx-0">
+    <div class="bg-dark text-white mx-0 px-12">
         <div class="container text-center">
             <div class="row justify-content-center">
-                <div class="col-3 mx-4 ">
+                <div class="col-3 mx-5 px-5">
                     <h2>Email</h2>
                     <p class="bg-purple">ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk membahas
                         tetang masalah,
                         pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda
                     <p>
                 </div>
-                <div class="col-3 mx-4 ">
+                <div class="col-3 mx-5 px-5">
                     <h2>Email</h2>
                     <p class="bg-purple">ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk membahas
                         tetang masalah,
                         pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda
                     <p>
                 </div>
-                <div class="col-3 mx-4 ">
+                <div class="col-3 mx-5 px-5">
                     <h2>Email</h2>
                     <p class="bg-purple">ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk membahas
                         tetang masalah,
@@ -31,14 +31,14 @@
         </div>
         <div class="container text-center">
             <div class="row justify-content-center">
-                <div class="col-3 mx-4 ">
+                <div class="col-3 mx-5 px-5">
                     <h2>Email</h2>
                     <p class="bg-purple">ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk
                         membahas tetang masalah,
                         pembayaran, dan pertanyaan lain nya. Operator kami akan siap selama 24 jam untuk melayani anda
                     <p>
                 </div>
-                <div class="col-3 mx-4 ">
+                <div class="col-3 mx-5 px-5">
                     <h2>Email</h2>
                     <p class="bg-purple">ini adalah email kami, kalian bisa menghubungi kami melalui email ini untuk
                         membahas tetang masalah,
