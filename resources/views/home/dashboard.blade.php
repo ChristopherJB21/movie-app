7f0ce102d5f66bce7574c66310547259fb07f6bd
MarioAnanta <anantamario1@gmail.com>
2023-06-11 16:59:56 +0700

    mario
    on progress

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -1,10 +1,42 @@
 @extends('layouts.app')
 @section('content')
-    <div class = "Home">
-        <div class = "Promotion">
-            <a href="linkpromot">
-            <img src="images/movieAssets/PROMO 1.png" class="img-fluid" alt="Responsive image">
+        <div class="container">
+            <div class="row">
+                <div>
+                    <a href="LinkPromo">
+                        <img src="images/movieAssets/PROMO 1.png" class="img-fluid" alt="Responsive image">
+                    </a>
+                </div>
+            </div>
         </div>
-        
+
+        <div class="container">
+            <div class="flex-column container">
+                <div class="row">
+                    <div class="my-3">
+                        <h1>New Arrival</h1>
+                        <div class="container">
+                            <div class="row">
+                              <div class="col-sm">
+                                <a href="">
+                                    <img src="images/movieAssets/anime-poster-K86JWC.jpg" class="img-fluid" alt="Responsive image">
+                                </a>
+                              </div>
+                              <div class="col-sm">
+                                <a href="">
+                                    <img src="images/movieAssets/anime-poster-K86JWC.jpg" class="img-fluid" alt="Responsive image">
+                                </a>
+                              </div>
+                              <div class="col-sm">
+                                <a href="">
+                                    <img src="images/movieAssets/anime-poster-K86JWC.jpg" class="img-fluid" alt="Responsive image">
+                                </a>
+                              </div>
+                            </div>
+                          </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
