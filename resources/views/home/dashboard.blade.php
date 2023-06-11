b8ac76b8877f1952a57e7da832a02879f9959f2c
MarioAnanta <anantamario1@gmail.com>
2023-06-11 09:43:13 +0700

    Mario (promote tab)
    on progress

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.app')
 @section('content')
-
+    <div class = "Home">
+        <div class = "Promotion">
+            <a href="linkpromot">
+            <img src="images/movieAssets/PROMO 1.png" class="img-fluid" alt="Responsive image">
+        </div>
+        
+    </div>
 @endsection
