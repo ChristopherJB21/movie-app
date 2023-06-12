77a261984b7edf9b498984cbc7e0acf33c67a154
Christopher Reswara <c1kalijaten@gmail.com>
2023-06-12 20:51:23 +0700

    Restore AboutUs

--- resources/views/home/aboutUs.blade.php
+++ resources/views/home/aboutUs.blade.php
@@ -1,3 +1,101 @@
 @extends('layouts.app')
 @section('content')
+
+<!-- CODING DI SINI -->
+<div class="d-flex justify-content-center my-4"> 
+
+  <div id="carouselExampleControlsNoTouching" class="carousel slide w-80" data-bs-touch="false" data-bs-interval="false">
+    <div class="carousel-inner   ">
+      <div class="carousel-item active ">
+        <img src="https://t-2.tstatic.net/medan/foto/bank/images/Bioskop-Cinema-XXI-The-Manhattan.jpg" class="d-block w-full " alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://t-2.tstatic.net/medan/foto/bank/images/Bioskop-Cinema-XXI-The-Manhattan.jpg" class="d-block w-full" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://t-2.tstatic.net/medan/foto/bank/images/Bioskop-Cinema-XXI-The-Manhattan.jpg" class="d-block w-full" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</div>
+
+<div class="row justify-content-center m-0 p-0">
+    <p class=" col-8  text-justify" style="text-align: justify; text-justify: inter-word;">
+    Selamat datang di Movie App, destinasi online terbaik untuk pembelian tiket aplikasi. Kami adalah platform inovatif yang didedikasikan untuk memudahkan Anda dalam membeli tiket untuk berbagai acara dan hiburan di seluruh dunia.
+    <br>
+    <br>
+    Visi kami adalah menghadirkan pengalaman yang luar biasa bagi pengguna kami. Kami percaya bahwa setiap momen berharga dalam hidup Anda layak diisi dengan kegembiraan dan kesenangan. Oleh karena itu, kami telah berkomitmen untuk menyediakan akses mudah, cepat, dan aman ke berbagai tiket acara langsung, konser, festival musik, pertandingan olahraga, teater, bioskop, dan banyak lagi.
+    <br>
+    <br>
+    Apa yang membedakan kami adalah kemudahan penggunaan platform kami. Kami telah merancang situs web dan aplikasi kami dengan antarmuka yang intuitif dan ramah pengguna, sehingga Anda dapat dengan mudah menavigasi, mencari, dan membeli tiket yang Anda inginkan. Kami juga menawarkan berbagai opsi pembayaran yang aman dan fleksibel, sehingga Anda dapat menyesuaikan dengan preferensi Anda sendiri.
+    <br>
+    <br>
+    Kami bermitra dengan penyelenggara acara terkemuka dan agen tiket yang terpercaya untuk memastikan bahwa Anda selalu mendapatkan tiket asli dan resmi dengan harga yang kompetitif. Dalam upaya kami untuk memberikan layanan terbaik kepada pelanggan, kami menyediakan informasi terkini tentang acara, jadwal pertunjukan, pembaruan artis, dan ulasan pengguna, sehingga Anda dapat membuat keputusan yang terinformasi sebelum membeli tiket.
+    <br>
+    <br>
+    Kami sangat menghargai umpan balik dan masukan dari pelanggan kami. Tim dukungan pelanggan kami tersedia untuk membantu menjawab pertanyaan Anda, menyelesaikan masalah, atau memberikan saran sepanjang waktu. Kepuasan Anda adalah prioritas kami, dan kami berusaha untuk terus meningkatkan layanan kami.
+    <br>
+    <br>
+    Terima kasih telah memilih Movie App sebagai mitra pembelian tiket Anda. Kami berharap Anda menemukan pengalaman yang menyenangkan dan tak terlupakan di acara yang Anda pilih. Jika Anda memiliki pertanyaan lebih lanjut atau membutuhkan bantuan, jangan ragu untuk menghubungi kami.
+    </p>
+    
+</div>
+@endsection@extends('layouts.app')
+@section('content')
+
+<!-- CODING DI SINI -->
+<div class="d-flex justify-content-center my-4"> 
+
+  <div id="carouselExampleControlsNoTouching" class="carousel slide w-80" data-bs-touch="false" data-bs-interval="false">
+    <div class="carousel-inner   ">
+      <div class="carousel-item active ">
+        <img src="https://t-2.tstatic.net/medan/foto/bank/images/Bioskop-Cinema-XXI-The-Manhattan.jpg" class="d-block w-full " alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://t-2.tstatic.net/medan/foto/bank/images/Bioskop-Cinema-XXI-The-Manhattan.jpg" class="d-block w-full" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://t-2.tstatic.net/medan/foto/bank/images/Bioskop-Cinema-XXI-The-Manhattan.jpg" class="d-block w-full" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</div>
+
+<div class="row justify-content-center m-0 p-0">
+    <p class=" col-8  text-justify" style="text-align: justify; text-justify: inter-word;">
+    Selamat datang di Movie App, destinasi online terbaik untuk pembelian tiket aplikasi. Kami adalah platform inovatif yang didedikasikan untuk memudahkan Anda dalam membeli tiket untuk berbagai acara dan hiburan di seluruh dunia.
+    <br>
+    <br>
+    Visi kami adalah menghadirkan pengalaman yang luar biasa bagi pengguna kami. Kami percaya bahwa setiap momen berharga dalam hidup Anda layak diisi dengan kegembiraan dan kesenangan. Oleh karena itu, kami telah berkomitmen untuk menyediakan akses mudah, cepat, dan aman ke berbagai tiket acara langsung, konser, festival musik, pertandingan olahraga, teater, bioskop, dan banyak lagi.
+    <br>
+    <br>
+    Apa yang membedakan kami adalah kemudahan penggunaan platform kami. Kami telah merancang situs web dan aplikasi kami dengan antarmuka yang intuitif dan ramah pengguna, sehingga Anda dapat dengan mudah menavigasi, mencari, dan membeli tiket yang Anda inginkan. Kami juga menawarkan berbagai opsi pembayaran yang aman dan fleksibel, sehingga Anda dapat menyesuaikan dengan preferensi Anda sendiri.
+    <br>
+    <br>
+    Kami bermitra dengan penyelenggara acara terkemuka dan agen tiket yang terpercaya untuk memastikan bahwa Anda selalu mendapatkan tiket asli dan resmi dengan harga yang kompetitif. Dalam upaya kami untuk memberikan layanan terbaik kepada pelanggan, kami menyediakan informasi terkini tentang acara, jadwal pertunjukan, pembaruan artis, dan ulasan pengguna, sehingga Anda dapat membuat keputusan yang terinformasi sebelum membeli tiket.
+    <br>
+    <br>
+    Kami sangat menghargai umpan balik dan masukan dari pelanggan kami. Tim dukungan pelanggan kami tersedia untuk membantu menjawab pertanyaan Anda, menyelesaikan masalah, atau memberikan saran sepanjang waktu. Kepuasan Anda adalah prioritas kami, dan kami berusaha untuk terus meningkatkan layanan kami.
+    <br>
+    <br>
+    Terima kasih telah memilih Movie App sebagai mitra pembelian tiket Anda. Kami berharap Anda menemukan pengalaman yang menyenangkan dan tak terlupakan di acara yang Anda pilih. Jika Anda memiliki pertanyaan lebih lanjut atau membutuhkan bantuan, jangan ragu untuk menghubungi kami.
+    </p>
+    
+</div>
 @endsection
