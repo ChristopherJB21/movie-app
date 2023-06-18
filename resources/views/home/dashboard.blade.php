44930a34b8b80f761eb7b3cd257d25ac1670d067
MarioAnanta <anantamario1@gmail.com>
2023-06-19 00:33:53 +0700

    Mario
    Progress

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -1,43 +1,79 @@
 @extends('layouts.app')
 @section('content')
-        <div class="container">
-            <div class="row">
+    <div class="home-content" style="margin-top: 10px;">
+        <div class="promotion" style="margin: 30px; min-height: 160px">
+            <div class="promotion">
                 <div>
                     <a href="LinkPromo">
-                        <img src="images/movieAssets/PROMO 1.png" class="img-fluid" alt="Responsive image">
+                        <img src="images/movieAssets/KODE PROMO.png" class="img-fluid" alt="Responsive image">
                     </a>
                 </div>
             </div>
         </div>
 
-        <div class="container">
-            <div class="flex-column container">
-                <div class="row">
-                    <div class="my-3">
-                        <h1>New Arrival</h1>
-                        <div class="container">
-                            <div class="row">
-                              <div class="col-sm">
-                                <a href="">
-                                    <img src="images/movieAssets/anime-poster-K86JWC.jpg" class="img-fluid" alt="Responsive image">
-                                </a>
-                              </div>
-                              <div class="col-sm">
-                                <a href="">
-                                    <img src="images/movieAssets/anime-poster-K86JWC.jpg" class="img-fluid" alt="Responsive image">
-                                </a>
-                              </div>
-                              <div class="col-sm">
-                                <a href="">
-                                    <img src="images/movieAssets/anime-poster-K86JWC.jpg" class="img-fluid" alt="Responsive image">
-                                </a>
-                              </div>
-                            </div>
-                          </div>
-                    </div>
+        <div class="text-center" >
+            <div class="grid_movie">
+                <a href="gui.movie_details.php?sid=&amp;movie_id=23ELEL">
+                <img src="images/movieAssets/anime-poster-K86JWC.jpg" style="max-height:500px">
+                </a>
+                <div class="clearfix "> </div>
+                <div class="title text-center">
+                    ELEMENTAL FORCES OF NATURE
                 </div>
+                <div class="btn-group-sm rating">
+                <span class="btn btn-default btn-outline disabled" style="color: #005350;">2D</span>
+                <a class="btn btn-default btn-outline disabled" style="color: #005350;">SU</a>
             </div>
         </div>
 
+        <div class="text-center" >
+            <div class="grid_movie">
+                <a href="gui.movie_details.php?sid=&amp;movie_id=23ELEL">
+                <img src="images/movieAssets/anime-poster-K86JWC.jpg" style="max-height:500px">
+                </a>
+                <div class="clearfix "> </div>
+                <div class="title text-center">
+                    TITLE
+                </div>
+                <div class="btn-group-sm rating">
+                <span class="btn btn-default btn-outline disabled" style="color: #005350;">2D</span>
+                <a class="btn btn-default btn-outline disabled" style="color: #005350;">SU</a>
+            </div>
+        </div>
+
+        <div class="text-center" >
+            <div class="grid_movie">
+                <a href="gui.movie_details.php?sid=&amp;movie_id=23ELEL">
+                <img src="images/movieAssets/anime-poster-K86JWC.jpg" style="max-height:500px">
+                </a>
+                <div class="clearfix "> </div>
+                <div class="title text-center">
+                    TITLE
+                </div>
+                <div class="btn-group-sm rating">
+                <span class="btn btn-default btn-outline disabled" style="color: #005350;">2D</span>
+                <a class="btn btn-default btn-outline disabled" style="color: #005350;">SU</a>
+            </div>
+        </div>
+
+        <div class="text-center" >
+            <div class="grid_movie">
+                <a href="gui.movie_details.php?sid=&amp;movie_id=23ELEL">
+                <img src="images/movieAssets/anime-poster-K86JWC.jpg" style="max-height:500px">
+                </a>
+                <div class="clearfix "> </div>
+                <div class="title text-center">
+                    TITLE
+                </div>
+                <div class="btn-group-sm rating">
+                <span class="btn btn-default btn-outline disabled" style="color: #005350;">2D</span>
+                <a class="btn btn-default btn-outline disabled" style="color: #005350;">SU</a>
+            </div>
+        </div>
+    </div>
+    
+        
+    
+        
         
 @endsection
