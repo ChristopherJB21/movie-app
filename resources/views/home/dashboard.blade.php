56b396b2d6122c3bee543718364b96aa0e4fbc4a
MarioAnanta <anantamario1@gmail.com>
2023-06-20 13:55:10 +0700

    Mario
    Progress

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -11,22 +11,21 @@
             </div>
         </div>
 
-        <div class="background">
-            <img src="images/movieAssets/PROMOTION.png" style="max-height:500px; background: no-repeat" alt="">
-            <div class="text-center" >
+        <div class="background" style="background-image: url(images/movieAssets/PROMOTION.png); height: 78vh; margin: 22px">
+            <div class="text-center">
                 <div class="text-center" >
                     <div class="grid_movie">
                     <a href="">
-                    <img src="images/movieAssets/THE FLASH.png" style="max-height:500px">
+                    <img src="images/movieAssets/THE FLASH.png" style="max-height: 500px; margin: 13px">
                     </a>
-                    <div class="clearfix "> </div>
+                    <div class="clearfix"> </div>
                     <div class="title text-center">
                         THE FLASH
                     </div>
-                    <div class="button" style="margin: 20px;">
+                    <div class="button">
                         <div class="btn-group-sm rating">
-                            <span class="btn btn-default btn-outline disabled" style="color: #005350;">2D</span>
-                            <a class="btn btn-default btn-outline disabled" style="color: #005350;">SU</a>
+                            <span class="btn btn-default btn-outline disabled" style="color: #ffffff;">2D</span>
+                            <a class="btn btn-default btn-outline disabled" style="color: #ffffff;">SU</a>
                         </div>
                     </div>
                     </div>
@@ -35,78 +34,70 @@
         </div>
 
 
-        <div class="text-center" >
-            <div class="text-center" >
-                <div class="grid_movie">
-                <a href="">
-                <img src="images/movieAssets/THE FLASH.png" style="max-height:500px">
-                </a>
-                <div class="clearfix "> </div>
-                <div class="title text-center">
-                    THE FLASH
-                </div>
-                <div class="button" style="margin: 20px;">
-                    <div class="btn-group-sm rating">
-                        <span class="btn btn-default btn-outline disabled" style="color: #005350;">2D</span>
-                        <a class="btn btn-default btn-outline disabled" style="color: #005350;">SU</a>
+        <div class="background" style="background-image: url(images/movieAssets/PROMOTION.png); height: 78vh; margin: 22px">
+            <div class="text-center">
+                <div class="text-center" >
+                    <div class="grid_movie">
+                    <a href="">
+                    <img src="images/movieAssets/TRANSFORMERS RISE OF THE BEASTS.jpg" style="max-height: 500px; margin: 13px">
+                    </a>
+                    <div class="clearfix"> </div>
+                    <div class="title text-center">
+                        TRANSFORMERS: RISE OF THE BEASTS
+                    </div>
+                    <div class="button">
+                        <div class="btn-group-sm rating">
+                            <span class="btn btn-default btn-outline disabled" style="color: #ffffff;">2D</span>
+                            <a class="btn btn-default btn-outline disabled" style="color: #ffffff;">SU</a>
+                        </div>
+                    </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="text-center" >
-            <div class="text-center" >
-                <div class="grid_movie">
-                <a href="">
-                <img src="images/movieAssets/THE CHILDE.jpg" style="max-height:500px">
-                </a>
-                <div class="clearfix "> </div>
-                <div class="title text-center">
-                    THE CHILDE
-                </div>
-                <div class="button" style="margin: 20px;">
-                    <div class="btn-group-sm rating">
-                        <span class="btn btn-default btn-outline disabled" style="color: #005350;">2D</span>
-                        <a class="btn btn-default btn-outline disabled" style="color: #005350;">SU</a>
+        <div class="background" style="background-image: url(images/movieAssets/PROMOTION.png); height: 78vh; margin: 22px">
+            <div class="text-center">
+                <div class="text-center" >
+                    <div class="grid_movie">
+                    <a href="">
+                    <img src="images/movieAssets/THE CHILDE.jpg" style="max-height: 500px; margin: 13px">
+                    </a>
+                    <div class="clearfix"> </div>
+                    <div class="title text-center">
+                        THE CHILDE
                     </div>
-                </div>
-            </div>
-        </div>
-            
-
-        <div class="text-center" >
-            <div class="grid_movie">
-                <a href="">
-                <img src="images/movieAssets/TRANSFORMERS RISE OF THE BEASTS.jpg" style="max-height:500px">
-                </a>
-                <div class="clearfix "> </div>
-                <div class="title text-center">
-                    TRANSFORMERS: RISE OF THE BEASTS
-                </div>
-                <div class="button" style="margin: 20px;">
-                    <div class="btn-group-sm rating">
-                        <span class="btn btn-default btn-outline disabled" style="color: #005350;">2D</span>
-                        <a class="btn btn-default btn-outline disabled" style="color: #005350;">SU</a>
+                    <div class="button">
+                        <div class="btn-group-sm rating">
+                            <span class="btn btn-default btn-outline disabled" style="color: #ffffff;">2D</span>
+                            <a class="btn btn-default btn-outline disabled" style="color: #ffffff;">SU</a>
+                        </div>
+                    </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="text-center" >
-            <div class="grid_movie">
-                <a href="">
-                <img src="images/movieAssets/THE BOOGEYMAN.jpg" style="max-height:500px">
-                </a>
-                <div class="clearfix "> </div>
-                <div class="title text-center">
-                    THE BOOGEYMAN
-                </div>
-                <div class="button" style="margin: 20px;">
-                    <div class="btn-group-sm rating">
-                        <span class="btn btn-default btn-outline disabled" style="color: #005350;">2D</span>
-                        <a class="btn btn-default btn-outline disabled" style="color: #005350;">SU</a>
+        <div class="background" style="background-image: url(images/movieAssets/PROMOTION.png); height: 78vh; margin: 22px">
+            <div class="text-center">
+                <div class="text-center" >
+                    <div class="grid_movie">
+                    <a href="">
+                    <img src="images/movieAssets/THE BOOGEYMAN.jpg" style="max-height: 500px; margin: 13px">
+                    </a>
+                    <div class="clearfix"> </div>
+                    <div class="title text-center">
+                        THE BOOGEYMAN
+                    </div>
+                    <div class="button">
+                        <div class="btn-group-sm rating">
+                            <span class="btn btn-default btn-outline disabled" style="color: #ffffff;">2D</span>
+                            <a class="btn btn-default btn-outline disabled" style="color: #ffffff;">SU</a>
+                        </div>
+                    </div>
                     </div>
                 </div>
             </div>
         </div>
+
 @endsection
